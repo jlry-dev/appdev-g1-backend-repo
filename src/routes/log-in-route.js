e61af4b1923099f36856926330a958715ce0922b
jlry <156220052+jlry-dev@users.noreply.github.com>
2025-03-18 14:45:46 +0800

    feat: added login verify route

--- src/routes/log-in-route.js
+++ src/routes/log-in-route.js
@@ -2,11 +2,17 @@ const { Router } = require('express')
 
 const controller = require('../controllers/log-in-controllers')
 const validator = require('../middlewares/validator')
+const verifyToken = require('./middlewares/jwt-verifier-middleware')
 
 const logInRouter = Router()
 
 // path prefix "/log-in"
 
 logInRouter.post('/', validator.logIn, controller.postLogIn)
+logInRouter.post('/verify', verifyToken, (req, res) => 
+    res.status(200).json({
+      status: "success",
+    })
+  }
 
 module.exports = logInRouter
