ed5cf10321d2403b33c0da66d567ac5d3b6031f1
jlry-dev <personal.jlry@gmail.com>
2025-03-26 17:54:06 +0800

    ref: changed DB config to use connection string on production and development

--- src/config/database.js
+++ src/config/database.js
@@ -1,17 +1,8 @@
 const { Pool } = require('pg')
 require('dotenv').config()
 
-const pool =
-    process.env.NODE_ENV === 'production'
-        ? new Pool({
-              connectionString: process.env.DB_CONNECTION_STRING,
-          })
-        : new Pool({
-              host: process.env.DB_HOST,
-              database: process.env.DB_NAME,
-              user: process.env.DB_USER,
-              port: process.env.DB_PORT,
-              password: process.env.DB_PASSWORD,
-          })
+const pool = new Pool({
+    connectionString: process.env.DB_CONNECTION_STRING,
+})
 
 module.exports = pool
