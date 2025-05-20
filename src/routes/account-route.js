fca0e96c586750aa451c9869f6626e36cfcddc4b
jlry <156220052+jlry-dev@users.noreply.github.com>
2025-05-20 16:59:21 +0800

    Update account-route.js

--- src/routes/account-route.js
+++ src/routes/account-route.js
@@ -6,17 +6,16 @@ const validator = require('../middlewares/validator')
 
 const accountRouter = Router()
 
-accountRouter.use(verifyToken)
-
-// prefix account/
-accountRouter.get("/info", controller.getInfo)
-accountRouter.post("/delete", controller.postDeleteAccount)
-accountRouter.post("/info/update", validator.updateInfo, controller.postInfoUpdate)
-
 accountRouter.post("/password/update", validator.updatePassword, controller.postNewPassword)
 accountRouter.post("/password/reset", validator.resetPassword, controller.postPasswordReset)
 accountRouter.post("/password/reset/request", validator.validateEmail, controller.postRequestReset)
 accountRouter.post("/password/reset/confirm", validator.validateEmail, controller.postConfirmReset)
+
+accountRouter.use(verifyToken)
+// prefix account/
+accountRouter.get("/info", controller.getInfo)
+accountRouter.post("/delete", controller.postDeleteAccount)
+accountRouter.post("/info/update", validator.updateInfo, controller.postInfoUpdate)
 
 
 
